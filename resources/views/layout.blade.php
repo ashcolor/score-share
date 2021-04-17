5a5b3370706ea3f9c0aacf8240dbdfbf18bbd8aa
ashcolor <ashcolor27@gmail.com>
2021-04-17 15:10:02 +0900

    add: larabel UI,Vue.jsの導入

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,10 +1,17 @@
 <!DOCTYPE HTML>
 <html>
-    <title>@yield('title')</title>
 <head>
-<body>
-    @include('header')
-    @yield('content')
-</body>
+    <title>@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
+<body>
+@include('header')
+@yield('content')
+<div id="app">
+    <example-component></example-component>
+</div>
+<script src=" {{ mix('js/app.js') }} "></script>
+</body>
+
 </html>
