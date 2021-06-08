7c8568563d46abe72f0373c099cb98b1c90d8cfd
ashcolor <ashcolor27@gmail.com>
2021-06-08 22:00:56 +0900

    add: authスキャフォールド追加

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('{any}', function () {
     return view('index');
 })->where('any', '.*');
